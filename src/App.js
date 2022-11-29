7797f04da8fa3a1064d0ea1d4d0031b5a6f1b145
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-29 09:30:57 -0300

    feat: create headline cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import HeadlineCards from "./components/HeadlineCards";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <HeadlineCards />
     </div>
   );
 }
