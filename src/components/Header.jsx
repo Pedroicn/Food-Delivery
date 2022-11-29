6fa4188e50a8de512fdd91c51ec0b3c631961577
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-28 23:59:42 -0300

    feat: create sidebar for mobile device

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { BsFillCartFill } from 'react-icons/bs';
+import { GiHamburgerMenu, GiWallet } from 'react-icons/gi';
+import { AiOutlineSearch, AiOutlineClose, AiFillHeart } from 'react-icons/ai';
+import { BsFillCartFill, BsFillArrowUpSquareFill } from 'react-icons/bs';
+import { TbTruckDelivery } from 'react-icons/tb';
+import { IoMdHelpCircle } from 'react-icons/io';
+import { FaUserFriends } from 'react-icons/fa';
+import { IoTicket } from 'react-icons/io5'
+
 const Header = () => {
   return (
     <header className='max-w-[1640px] mx-auto flex justify-between items-center p-3'>
@@ -25,6 +30,28 @@ const Header = () => {
       <button className='bg-black text-white hidden md:flex items-center py-2 rounded-full'>
         <BsFillCartFill size={20} className='mr-2' /> Carrinho
       </button>
+
+      {/* overlay menu, mobile */}
+      <div className='bg-black/80 fixed w-full h-screen top-0 left-0'>
+
+      </div>
+      {/* barra lateral */}
+      <aside className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'>
+        <AiOutlineClose size={30} className='absolute cursor-pointer right-4 top-4'/>
+        <h2 className='text-2xl p-4'>Food <span className='font-bold'>Delivery</span> </h2>
+        <nav>
+          <ul className='flex flex-col p-4 text-gray-800'>
+            <li className='flex px-4 py-4 text-xl'><TbTruckDelivery size={20} className='mr-4'/> Pedidos</li>
+            <li className='flex px-4 py-4 text-xl'><GiWallet size={20} className='mr-4' /> Carteira</li>
+            <li className='flex px-4 py-4 text-xl'><AiFillHeart size={20} className='mr-4' /> Favoritos</li>
+            <li className='flex px-4 py-4 text-xl'><IoMdHelpCircle size={20} className='mr-4' /> Ajuda</li>
+            <li className='flex px-4 py-4 text-xl'><IoTicket size={20} className='mr-4' /> Promoções</li>
+            <li className='flex px-4 py-4 text-xl'><BsFillArrowUpSquareFill size={20} className='mr-4' /> O melhor</li>
+            <li className='flex px-4 py-4 text-xl'><FaUserFriends size={20} className='mr-4' /> Convide amigos</li>
+          </ul>
+        </nav>
+      </aside>
+
     </header>
   )
 }
