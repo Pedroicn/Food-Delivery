d3d1b2955790c01f5db8dbcf4b702f3b869eb3ea
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-29 12:23:12 -0300

    feat: create fiter button

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import data from '../data/data';
 
 const Food = () => {
   return (
-    <div>Food</div>
+    <main className='max-w-[1640px] m-auto px-4 py-12'>
+      <h1 className='text-orange-700 font-bold text-4xl text-center'>Melhor avaliados</h1>
+      <div className='flex flex-col lg:flex-row justify-between'>
+
+        <div>
+          <p className='font-bold text-gray-700'>Filtrar</p>
+          <div className='flex justify-around flex-wrap'>
+            <button className='m-1 border-orange-700 text-orange-700 hover:bg-orange-700 hover:text-white'>Todos</button>
+            <button className='m-1 border-orange-700 text-orange-700 hover:bg-orange-700 hover:text-white'>Hambuguer</button>
+            <button className='m-1 border-orange-700 text-orange-700 hover:bg-orange-700 hover:text-white'>Pizza</button>
+            <button className='m-1 border-orange-700 text-orange-700 hover:bg-orange-700 hover:text-white'>Salada</button>
+            <button className='m-1 border-orange-700 text-orange-700 hover:bg-orange-700 hover:text-white'>Frango</button>
+          </div>
+        </div>
+
+        <div>
+          <p className='font-bold text-gray-700'>Filtrar por preço</p>
+          <div className='flex justify-around max-w-[390px] w-full'>
+            <button className='m-1 border-orange-700 text-orange-700 hover:bg-orange-700 hover:text-white'>$</button>
+            <button className='m-1 border-orange-700 text-orange-700 hover:bg-orange-700 hover:text-white'>$$</button>
+            <button className='m-1 border-orange-700 text-orange-700 hover:bg-orange-700 hover:text-white'>$$$</button>
+            <button className='m-1 border-orange-700 text-orange-700 hover:bg-orange-700 hover:text-white'>$$$$</button>
+          </div>
+        </div>
+
+      </div>
+    </main>
   )
 }
 
