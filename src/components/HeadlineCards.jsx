9abe6031de470d8f07c55311c3c8c4faee37f0ae
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-29 15:24:28 -0300

    feat: change the headline card name

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -7,7 +7,7 @@ const HeadlineCards = () => {
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
           <p className='font-bold text-2xl px-2 pt-4'>Promoção</p>
           <p className='px-2'>Até 28/12</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Pedir agora</button>
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
@@ -19,7 +19,7 @@ const HeadlineCards = () => {
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
           <p className='font-bold text-2xl px-2 pt-4'>Novos Restaurantes</p>
           <p className='px-2'>Adicionados Diariamennte</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Pedir agora</button>
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
@@ -32,7 +32,7 @@ const HeadlineCards = () => {
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
           <p className='font-bold text-2xl px-2 pt-4'>Entregamos sobremesas também</p>
           <p className='px-2'>As mais saborosas</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Pedir agora</button>
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
